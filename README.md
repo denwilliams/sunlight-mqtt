d0e562a016d4d07cad1f97a1d0ce5cf092669d16
Den Williams <work@denwilliams.net>
2018-06-27 23:28:18 +1000

    first version

---
+++ README.md
@@ -0,0 +1,30 @@
+# sunlight-mqtt
+
+Emit over MQTT when sunlight phases change (using SunCalc)
+
+## Topics
+
+Emits `prefix/night` and `prefix/dark` with data `{"value": true/false}` where prefix is set in config.
+
+## Running
+
+Install `npm i -g sunlight-mqtt`
+
+Create a YAML file somewhere. See `config.example.yml`
+
+Run (replace path)
+
+```
+CONFIG_PATH=./config.yml sunlight-mqtt
+```
+
+## Example Config
+
+```
+mqtt:
+  uri: mqtt://localhost
+  prefix: sunlight/
+service:
+  latitude: -37.814
+  longitude: 144.96332
+```
---
+++ main.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const suncalc = require('suncalc');
+const mqttusvc = require('mqtt-usvc');
+
+const nightState = {
+  nadir: 'night',
+  nightEnd: 'night',
+  nauticalDawn: 'night',
+  dawn: 'night',
+  sunrise: 'day',
+  sunriseEnd: 'day',
+  goldenHourEnd: 'day',
+  solarNoon: 'day',
+  goldenHour: 'day',
+  sunsetStart: 'day',
+  sunset: 'night',
+  dusk: 'night',
+  nauticalDusk: 'night',
+  night: 'night',
+};
+
+const darkState = {
+  nadir: 'dark',
+  nightEnd: 'dark',
+  nauticalDawn: 'dark',
+  dawn: 'dark',
+  sunrise: 'dark',
+  sunriseEnd: 'dark',
+  goldenHourEnd: 'light',
+  solarNoon: 'light',
+  goldenHour: 'dark',
+  sunsetStart: 'dark',
+  sunset: 'dark',
+  dusk: 'dark',
+  nauticalDusk: 'dark',
+  night: 'dark',
+};
+
+const service = mqttusvc.create();
+
+let lastNightState, lastDarkState;
+
+const handler = () => {
+  const now = new Date();
+  const times = suncalc.getTimes(now, service.config.latitude, service.config.longitude)
+
+  const pastKeys = Object.keys(times)
+  .sort((a, b) => times[a] - times[b])
+  .filter(key => now > times[key]);
+
+  const lastKey = (pastKeys.length)
+    ? pastKeys[pastKeys.length - 1]
+    : 'night';
+
+  const currentNightState = nightState[lastKey];
+  const currentDarkState = darkState[lastKey];
+
+  if (currentDarkState !== lastDarkState) {
+    service.send('dark', {value: currentDarkState === 'dark'});
+  }
+
+  if (currentNightState !== lastNightState) {
+    service.send('night', {value: currentNightState === 'night'});
+  }
+};
+
+handler();
+
+// need to set this more intelligently
+setInterval(handler, 60000);

0f8d708318052460d2e1e50d58c2e81e24c88204
Den Williams <work@denwilliams.net>
2020-04-26 20:54:23 +1000

    try with v3 usvc

--- main.js
+++ main.js
@@ -1,73 +1,90 @@
 #!/usr/bin/env node
 
-const suncalc = require('suncalc');
-const mqttusvc = require('mqtt-usvc');
+const suncalc = require("suncalc");
+const mqttusvc = require("mqtt-usvc");
 
 const nightState = {
-  nadir: 'night',
-  nightEnd: 'night',
-  nauticalDawn: 'night',
-  dawn: 'night',
-  sunrise: 'day',
-  sunriseEnd: 'day',
-  goldenHourEnd: 'day',
-  solarNoon: 'day',
-  goldenHour: 'day',
-  sunsetStart: 'day',
-  sunset: 'night',
-  dusk: 'night',
-  nauticalDusk: 'night',
-  night: 'night',
+  nadir: "night",
+  nightEnd: "night",
+  nauticalDawn: "night",
+  dawn: "night",
+  sunrise: "day",
+  sunriseEnd: "day",
+  goldenHourEnd: "day",
+  solarNoon: "day",
+  goldenHour: "day",
+  sunsetStart: "day",
+  sunset: "night",
+  dusk: "night",
+  nauticalDusk: "night",
+  night: "night"
 };
 
 const darkState = {
-  nadir: 'dark',
-  nightEnd: 'dark',
-  nauticalDawn: 'dark',
-  dawn: 'dark',
-  sunrise: 'dark',
-  sunriseEnd: 'dark',
-  goldenHourEnd: 'light',
-  solarNoon: 'light',
-  goldenHour: 'dark',
-  sunsetStart: 'dark',
-  sunset: 'dark',
-  dusk: 'dark',
-  nauticalDusk: 'dark',
-  night: 'dark',
+  nadir: "dark",
+  nightEnd: "dark",
+  nauticalDawn: "dark",
+  dawn: "dark",
+  sunrise: "dark",
+  sunriseEnd: "dark",
+  goldenHourEnd: "light",
+  solarNoon: "light",
+  goldenHour: "dark",
+  sunsetStart: "dark",
+  sunset: "dark",
+  dusk: "dark",
+  nauticalDusk: "dark",
+  night: "dark"
 };
 
-const service = mqttusvc.create();
+async function main() {
+  const service = await mqttusvc.create();
 
-let lastNightState, lastDarkState;
+  let lastNightState, lastDarkState;
 
-const handler = () => {
-  const now = new Date();
-  const times = suncalc.getTimes(now, service.config.latitude, service.config.longitude)
+  const handler = () => {
+    const now = new Date();
+    const times = suncalc.getTimes(
+      now,
+      service.config.latitude,
+      service.config.longitude
+    );
 
-  const pastKeys = Object.keys(times)
-  .sort((a, b) => times[a] - times[b])
-  .filter(key => now > times[key]);
+    const pastKeys = Object.keys(times)
+      .sort((a, b) => times[a] - times[b])
+      .filter(key => now > times[key]);
 
-  const lastKey = (pastKeys.length)
-    ? pastKeys[pastKeys.length - 1]
-    : 'night';
+    const lastKey = pastKeys.length ? pastKeys[pastKeys.length - 1] : "night";
 
-  const currentNightState = nightState[lastKey];
-  const currentDarkState = darkState[lastKey];
+    const currentNightState = nightState[lastKey];
+    const currentDarkState = darkState[lastKey];
 
-  if (currentDarkState !== lastDarkState) {
-    service.send('dark', {value: currentDarkState === 'dark'}, { retain: true });
-    lastDarkState = currentDarkState;
-  }
+    if (currentDarkState !== lastDarkState) {
+      service.send(
+        "dark",
+        { value: currentDarkState === "dark" },
+        { retain: true }
+      );
+      lastDarkState = currentDarkState;
+    }
 
-  if (currentNightState !== lastNightState) {
-    service.send('night', {value: currentNightState === 'night'}, { retain: true });
-    lastNightState = currentNightState;
-  }
-};
+    if (currentNightState !== lastNightState) {
+      service.send(
+        "night",
+        { value: currentNightState === "night" },
+        { retain: true }
+      );
+      lastNightState = currentNightState;
+    }
+  };
 
-handler();
+  handler();
 
-// need to set this more intelligently
-setInterval(handler, 60000);
+  // need to set this more intelligently
+  setInterval(handler, 60000);
+}
+
+main().catch(err => {
+  console.error(err.stack);
+  process.exit(1);
+});
